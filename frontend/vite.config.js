24c4fe38f6c843d92e43ba56d6a94102889fbbad
gyash1512 <gyash.1512@gmail.com>
2025-01-31 19:59:36 +0530

    refresh issue check

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
-    outDir: 'build' // Change output folder to "build"
+    outDir: 'dist',
   },
   server: {
     historyApiFallback: true,
   }
-})
+});
